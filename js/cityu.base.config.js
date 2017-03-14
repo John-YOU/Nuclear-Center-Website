2e096e7af7ab7697a1bc1d50423bbc1a2acb75e6
John-YOU <yhlfc@hotmail.com>
2017-03-14 22:27:41 +0800

    first

---
+++ js/cityu.base.config.js
@@ -0,0 +1,7 @@
+/* City University of Hong Kong */
+/* JavaScript Base Config */
+
+
+var cityuMobileWeb = true; // Enable mobile web {true|false}
+var cityuMobileTest = false; // Enable mobile web testing in desktop {true|false}
+var cityuMobileCSSPath = ''; // File path if you have a customized CSS for mobile web (e.g. 'css/cityu.mobile.custom.css')
